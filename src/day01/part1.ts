83396eb1a09c567192ebf8ae9b0a20773cb858d1
astya965 <astya965@gmail.com>
2022-12-02 14:51:55 +0100

    fix: day01 refactoring

--- src/day01/part1.ts
+++ src/day01/part1.ts
@@ -1,29 +1,33 @@
 import { parseInput } from "../util";
 
 const input = parseInput({
-  split: { delimiter: "\n", mapper: (e: any) => Number(e)},
+  split: { delimiter: "\n", mapper: (e: string) => Number(e) },
 });
+
+// Sum calories for each elf
+const getCaloriesSum = (caloriesArr: number[]) =>
+  caloriesArr.reduce((acc, curr) => acc + curr, 0);
 
 // Separate initial array into arrays with calories for each elf
 const findElfsCalories = (initialArray: number[]) => {
-    let i = 0;
-    const subarrays = [];
+  let i = 0;
+  const subarrays = [];
 
-    while (i != - 1) { 
-        i = initialArray.findIndex(v => v === 0);
-        if (i != - 1) { //If array contains delimiter
-            const removedItems = initialArray.splice(0, i + 1);
-            subarrays.push(removedItems);
-        } else if (initialArray.length > 0) { //Add last part
-            subarrays.push(initialArray);
-        }
+  while (i != -1) {
+    i = initialArray.findIndex((v) => v === 0);
+    if (i != -1) {
+      //If array contains delimiter
+      const removedItems = initialArray.splice(0, i + 1);
+      subarrays.push(getCaloriesSum(removedItems));
+    } else if (initialArray.length > 0) {
+      //Add last part
+      subarrays.push(getCaloriesSum(initialArray));
     }
+  }
 
-    // Sum calories for each elf
-    return subarrays.map(subarray => subarray.reduce((acc, curr) => acc + curr, 0));
-}
+  return subarrays;
+};
 export const caloriesArr = findElfsCalories(input);
-
 
 // Find maxs
 const max = Math.max(...caloriesArr);
